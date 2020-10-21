5fba67090c8ff3aa47c6baf108c9d78de005ec10
shread1234 <shread1234@hotmail.co.uk>
2020-10-21 18:30:34 +0100

    ability to remove role and more logging included

--- bot.js
+++ bot.js
@@ -5,23 +5,36 @@ const Discord = require("discord.js")
 const bot = new Discord.Client()
 
 const botListeningChannel = "dev-test-steamid"
+const toAllocatePointsRole = 'devtest-role'
 
 function correctMessageChannel(channelName) {
   return channelName === botListeningChannel
 }
 
-bot.on("ready", () => {
-  console.log("Bot ready")
+bot.on('ready', () => {
+  console.log('Bot ready')
 })
 
 bot.on("message", async (message) => {
   if (!correctMessageChannel(message.channel.name)) {
     return
   }
-  if (message.content.includes("devTest")) {
-    const steamId = message.content.split(" ")[1]
-    await connectAndSendPoints(steamId)
-    message.channel.send("test complete")
+  if (message.content.includes("devtest")) {
+      const pointsToAllocateRole = message.guild.roles.cache.find((role) => role.name === toAllocatePointsRole)
+      try {
+        const steamId = message.content.split(" ")[1]
+        // is ID in db?
+        // if: throw error and message me and luke
+        // else continue
+        await connectAndSendPoints(steamId)
+        // add ID to db
+        message.member.roles.remove(pointsToAllocateRole)
+        message.channel.send('devtest-role removed')
+        message.channel.send('test complete')
+      } catch (error) {
+        console.log(error)
+        message.channel.send(`Something went wrong allocating points. Error: ${error.message.replace(/Error:/, '')}`)
+      }
   }
 })
 
--- rcon.js
+++ rcon.js
@@ -2,30 +2,26 @@ require("dotenv").config()
 const { Rcon } = require("rcon-client")
 
 const rconCystalIsles = new Rcon({
-  host: process.env.RCON_CENTER_HOST,
-  port: process.env.RCON_CENTER_PORT,
+  host: process.env.RCON_CRYSTAL_HOST,
+  port: process.env.RCON_CRYSTAL_PORT,
   password: process.env.RCON_ALL_SERVERS_PASSWORD,
 })
 
 async function giveSteamIDPoints(steamId, server) {
   server.on("authenticated", () => console.log("Server connection authenticated"))
   server.on("error", (error) => {
+    server.end()
     throw error
   })
   console.log("connecting...")
   await server.connect()
   console.log("connected!")
-  const test = await server.send(`ScriptCommand TCsAR AddArcTotal ${steamId} 5000`)
-  console.log(test)
+  await server.send(`ScriptCommand TCsAR AddArcTotal ${steamId} 5000`)
   server.end()
 }
 
 async function connectAndSendPoints(steamId) {
-  try {
-    await giveSteamIDPoints(steamId, rconCystalIsles)
-  } catch (error) {
-    console.log(`Error giving points: ${error}`)
-  }
+  await giveSteamIDPoints(steamId, rconCystalIsles)
 }
 
 module.exports = { connectAndSendPoints }
