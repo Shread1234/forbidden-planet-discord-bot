24cb0b6d0491d2102da85a8c3d4cb14f1d088736
shread1234 <shread1234@hotmail.co.uk>
2020-12-04 17:27:02 +0000

    fix typo

--- API/express/src/contollers/steamIdController.js
+++ API/express/src/contollers/steamIdController.js
@@ -10,11 +10,12 @@ exports.getSteamId = async (req, res) => {
     const selectResult = await selectSteamId(steamId)
     if(!selectResult.length) {
     res.status(404).send('steamId not found')
+    return
     } else {
     res.status(200).send(selectResult)
     }
   } catch (error) {
-    res.status(400).res.send(error)
+    res.status(400).send(error)
   }
 }
 
