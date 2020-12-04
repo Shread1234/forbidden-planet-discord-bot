237af55b92557b1f91fb5ca52d464b9a41b3931a
shread1234 <shread1234@hotmail.co.uk>
2020-12-04 17:13:08 +0000

    improve return values in controller

--- API/express/src/contollers/steamIdController.js
+++ API/express/src/contollers/steamIdController.js
@@ -3,17 +3,18 @@ const { selectSteamId, insertSteamId } = require('../models/steamIdModel')
 exports.getSteamId = async (req, res) => {
   const { steamId } = req.query
   if(!steamId.length) {
-    return res.status(400).send('Invalid Steam ID')
+    res.status(400).send('Invalid Steam ID')
+    return
   }
   try {
     const selectResult = await selectSteamId(steamId)
     if(!selectResult.length) {
-      return res.status(404).send('steamId not found')
+      res.status(404).send('steamId not found')
     } else {
-      return res.status(200).send(selectResult)
+       res.status(200).send(selectResult)
     }
   } catch (error) {
-    return res.status(400).res.send(error)
+     res.status(400).res.send(error)
   }
 }
 
@@ -25,8 +26,8 @@ exports.postSteamId = async (req, res) => {
   }
   try {
     await insertSteamId(steamId)
-    return res.status(200).send(`Steam id ${steamId} was successfully added to the database.`)
+     res.status(200).send(`Steam id ${steamId} was successfully added to the database.`)
   } catch (error) {
-    return res.status(400).res.send(error)
+     res.status(400).res.send(error)
   }
 }
