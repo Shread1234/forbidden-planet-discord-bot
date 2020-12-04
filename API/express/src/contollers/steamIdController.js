e8219123ea132306bf90c631467d40caa0168c70
shread1234 <shread1234@hotmail.co.uk>
2020-12-04 17:22:57 +0000

    please work heroku

--- API/express/src/contollers/steamIdController.js
+++ API/express/src/contollers/steamIdController.js
@@ -9,12 +9,12 @@ exports.getSteamId = async (req, res) => {
   try {
     const selectResult = await selectSteamId(steamId)
     if(!selectResult.length) {
-      res.status(404).send('steamId not found')
+    res.status(404).send('steamId not found')
     } else {
-       res.status(200).send(selectResult)
+    res.status(200).send(selectResult)
     }
   } catch (error) {
-     res.status(400).res.send(error)
+    res.status(400).res.send(error)
   }
 }
 
@@ -26,8 +26,8 @@ exports.postSteamId = async (req, res) => {
   }
   try {
     await insertSteamId(steamId)
-     res.status(200).send(`Steam id ${steamId} was successfully added to the database.`)
+    res.status(200).send(`Steam id ${steamId} was successfully added to the database.`)
   } catch (error) {
-     res.status(400).res.send(error)
+    res.status(400).res.send(error)
   }
 }
