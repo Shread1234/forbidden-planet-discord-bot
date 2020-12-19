5714af55276cef6844f72424f895eb6f826d965e
shread1234 <shread1234@hotmail.co.uk>
2020-12-19 08:25:37 +0000

    Add extra tables to db and improve script commands

---
+++ API/db/migrations/1603709705959_create-steamid-tables.js
@@ -0,0 +1,68 @@
+exports.up = pgm => {
+    pgm.createTable('crystal_isles_steamids', {
+    steam_id: 'varchar(1000)',
+    created_at: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+  }),
+  pgm.createTable('ragnarok_steamids', {
+    steam_id: 'varchar(1000)',
+    created_at: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+  }),
+  pgm.createTable('abberation_steamids', {
+    steam_id: 'varchar(1000)',
+    created_at: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+  }),
+  pgm.createTable('extinction_steamids', {
+    steam_id: 'varchar(1000)',
+    created_at: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+  }),
+  pgm.createTable('genesis_steamids', {
+    steam_id: 'varchar(1000)',
+    created_at: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+  }),
+  pgm.createTable('valguero_steamids', {
+    steam_id: 'varchar(1000)',
+    created_at: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+  }),
+  pgm.createTable('test_steamids', {
+    steam_id: 'varchar(1000)',
+    created_at: {
+      type: 'timestamp',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+  })
+};
+
+exports.down = pgm => {
+  pgm.dropTable('crystal_isles_steamids'),
+  pgm.dropTable('ragnarok_steamids'),
+  pgm.dropTable('abberation_steamids'),
+  pgm.dropTable('extinction_steamids'),
+  pgm.dropTable('genesis_steamids'),
+  pgm.dropTable('valguero_steamids'),
+  pgm.dropTable('test_steamids')
+};
